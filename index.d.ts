38be44a3bb2c4f84fe0b69a3a97dc4b697d9715d
Ruben Rutten <ruben@rubenrutten.nl>
2019-01-31 14:12:00 +0100

    Fixed missing / wrong typings

--- index.d.ts
+++ index.d.ts
@@ -4,13 +4,18 @@ declare module "es6-request" {
 
     interface CustomOptions {
         bodyAsBuffer?: boolean;
+        returnBody?: boolean;
     }
 
     interface Options extends http.ClientRequestArgs {
         custom?: CustomOptions;
     }
+    
+    type BodyType = string | Buffer;
 
     class ES6Request {
+        req: http.ClientRequest;
+
         headers(obj: http.OutgoingHttpHeaders): ES6Request;
         header(key: string, value: number | string | string[] | undefined): ES6Request;
         authBasic(username: string, password: string): ES6Request;
@@ -23,7 +28,13 @@ declare module "es6-request" {
         then(onSuccess?: (data: [string | Buffer, http.IncomingMessage]) => any, onFailure?: (err: Error) => any): Promise<any>;
         catch(onFailure?: (err: Error) => any): Promise<any>;
         destroy(error?: Error): ES6Request;
-        perform(): Promise<any>;
+        perform(): Promise<[string | Buffer, http.IncomingMessage]>;
+        perform<T extends BodyType>(): Promise<[T, http.IncomingMessage]>;
+        on(event: "error", listener: (err: Error) => void): ES6Request;
+        on(event: "data", listener: (chunk: Buffer) => void): ES6Request;
+        on(event: "progress", listener: (progress: number, current: number, total: number) => void): ES6Request;
+        on(event: "end", listener: () => void): ES6Request;
+        on(event: "close", listener: () => void): ES6Request;
         write(chunk: string | Buffer, encoding?: string, callback?: Function): ES6Request;
         pipe(dest: stream.Writable, opt?: object): ES6Request;
         send(body: string | Buffer, encoding?: string, callback?: Function): Promise<any>;
